3cd8e523ff6dcb99ff15be12344da40674f31d2b
Caitano <josiasmartinscaitano@gmail.com>
2021-10-31 09:03:43 -0300

    Começando com a linguagem c

---
+++ adivinhacao/programa.c
@@ -0,0 +1,24 @@
+// GCC: é a plataforma de compila o código para c
+// -o: onde vai escrever o código binário, que sua maquina entende
+
+// escrever um texto: abre aspas na linguagem c
+// printf: usado para imrimir na tela
+// int main() {}: para começar escrever em C, precisa desse cara
+//  :preciso incluir a biblioteca que tem esse cara.
+// studio. (io: input e output)
+
+// precisa do #include para incluir a biblioteca studio.h
+
+// \n: usado para dar um enter no C
+#include <stdio.h>
+
+// é a função principal
+int main() {
+    printf("Bem vindo ao nosso jogo de adivinhação");
+
+    int chute;
+    // printf(): função que imprime
+    printf("Qual é o seu chute?");
+    // scanf(): função que lê
+    scanf()
+}
